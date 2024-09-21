9c5cca77ab1184c9d3b1dbda7c2e9280f0184125
Facundo <facundo.sydransky@ing.austral.edu.ar>
2024-09-20 21:21:55 -0300

    investment Ui and pictures

--- app/src/main/java/com/example/investment/investment/InvestmentHome.kt
+++ app/src/main/java/com/example/investment/investment/InvestmentHome.kt
@@ -1,14 +1,22 @@
 package com.example.investment.investment
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,32 +24,47 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.investment.R
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.investment.navigation.InvestmentScreen
 
 
 @Composable
 fun InvestmentHome(navController: NavHostController){
+    val forexUrls = listOf(R.drawable.dolar,R.drawable.euros,R.drawable.yenes)
+    val stockUrsl = listOf(R.drawable.google,R.drawable.msft_logo,R.drawable.applelogo)
+    val rawsUrls = listOf(R.drawable.gold,R.drawable.oil,R.drawable.trigo)
+    val cryptoUrls = listOf(R.drawable.ethereum,R.drawable.bitcoin,R.drawable.solana)
     Surface(modifier = Modifier.fillMaxSize()) {
         Text(text = "Simulate Investments")
         Column(modifier = Modifier.height(150.dp),
             verticalArrangement = Arrangement.Center
             ){
             InvestmentCard("Stocks",
-                onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)}
+                onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)},
+                stockUrsl
                     )
             InvestmentCard("Crypto",
-                onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)}
+                onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)},
+                cryptoUrls
                     )
             InvestmentCard("National Treasures",
-                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)}
+                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)},
+                stockUrsl
                 )
             InvestmentCard("Forex",
-                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)}
+                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)},
+                forexUrls
                 )
             InvestmentCard("Raws",
-                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)}
+                    onNavigate = { navController.navigate(InvestmentScreen.Simulation.name)},
+                rawsUrls
                 )
         }
     }
@@ -49,27 +72,65 @@ fun InvestmentHome(navController: NavHostController){
 
 
 @Composable
-fun InvestmentCard(investmentType: String, onNavigate: () -> Unit){
-    Card(onClick = {  },
+fun InvestmentCard(investmentType: String, onNavigate: () -> Unit, urls : List<Int>){
+    Card(
         modifier = Modifier
             .padding(8.dp)
             .padding(horizontal = 8.dp)
             .wrapContentHeight()
-            .height(100.dp)
-            .fillMaxWidth()) {
-        Text(text = investmentType, modifier = Modifier.padding(5.dp))
+            .height(120.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(id = R.color.lightgolden)
+        ),
+    )  {
+        Row (horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = investmentType,
+                modifier = Modifier.padding(5.dp),
+                fontSize = 18.sp,
+                fontFamily = FontFamily.Serif)
+        }
         Column(modifier = Modifier
             .height(80.dp)
             .padding(5.dp),
             verticalArrangement = Arrangement.Bottom,
 
         ){
-            Text(text = stringResource(id = R.string.investment_card_simulation),
-                color = colorResource(id = R.color.purple_500),
-                modifier = Modifier.clickable {
-                    onNavigate()
+            Row (horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier.fillMaxWidth()){
+                    urls.forEach{it -> CardImage(imageUrl = it)}
                 }
+                Spacer(modifier = Modifier.height(6.dp))
+            Row (modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center){
+                Text(
+                    text = stringResource(id = R.string.investment_card_simulation),
+                    color = colorResource(id = R.color.black),
+                    fontFamily = FontFamily.Serif,
+                    modifier = Modifier.clickable {
+                        onNavigate()
+                    }
                 )
+            }
         }
+    }
+}
+
+@Composable()
+fun CardImage(imageUrl: Int ){
+    Box(
+        modifier = Modifier
+            .size(45.dp)
+            .clip(CircleShape)
+            .background(Color.LightGray)
+    ) {
+        Image(
+            painter = painterResource(imageUrl),
+            contentDescription = "Euro Dollar",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
     }
 }
--- app/src/main/java/com/example/investment/investment/InvestmentSimulation.kt
+++ app/src/main/java/com/example/investment/investment/InvestmentSimulation.kt
@@ -1,5 +1,6 @@
 package com.example.investment.investment
 
+import android.util.Log
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -7,16 +8,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -27,6 +32,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -38,34 +44,50 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.investment.investment.viewModel.AnalyticsResponse
+import com.example.investment.investment.viewModel.SimulationModel
 
 @Preview
 @Composable
-fun SimulationHome(){
-    Surface (modifier = Modifier.fillMaxSize()){
-       Column {
-           SelectSimulationDate()
-           ShowInvestmentOptions()
-           ShowSimulationResult()
-       }
+fun SimulationHome() {
+    val viewModel: SimulationModel = hiltViewModel<SimulationModel>()
+    var selectedDate by remember { mutableStateOf("") } // Variable para almacenar la fecha seleccionada
+    var selectedCompanies by remember { mutableStateOf(listOf<String>()) } // Variable para almacenar las empresas seleccionadas
+    val analytics by viewModel.analytics.collectAsState()
+
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+            SelectSimulationDate(selectedDate) { newDate ->
+                selectedDate = newDate
+            }
+            ShowInvestmentOptions(selectedCompanies) { newCompanies ->
+                selectedCompanies = newCompanies
+            }
+            Button(
+                onClick = {
+                    viewModel.simulateInvestment(selectedDate, selectedCompanies.joinToString(","))
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+                Text("Enviar")
+            }
+            ShowSimulationResult(analytics)
+        }
     }
 }
 
 
 
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SelectSimulationDate(){
+fun SelectSimulationDate(selectedDate: String, onDateSelected: (String) -> Unit) {
     var showDatePicker by remember { mutableStateOf(false) }
     val datePickerState = rememberDatePickerState()
-    val selectedDate = datePickerState.selectedDateMillis?.let {
-        convertMillisToDate(it)
-    } ?: ""
+
 
     Box(
         modifier = Modifier.fillMaxWidth()
@@ -73,10 +95,10 @@ fun SelectSimulationDate(){
         OutlinedTextField(
             value = selectedDate,
             onValueChange = { },
-            label = { Text("DOB") },
+            label = { Text("DOI") },
             readOnly = true,
             trailingIcon = {
-                IconButton(onClick = { showDatePicker = !showDatePicker }) {
+                IconButton(onClick = { showDatePicker = true }) {
                     Icon(
                         imageVector = Icons.Default.DateRange,
                         contentDescription = "Select date"
@@ -87,6 +109,7 @@ fun SelectSimulationDate(){
                 .fillMaxWidth()
                 .height(64.dp)
         )
+
 
         if (showDatePicker) {
             Popup(
@@ -99,82 +122,152 @@ fun SelectSimulationDate(){
                         .offset(y = 64.dp)
                         .shadow(elevation = 4.dp)
                         .background(MaterialTheme.colorScheme.surface)
-                        .padding(16.dp)
+                        .padding(30.dp)
                 ) {
-                    DatePicker(
-                        state = datePickerState,
-                        showModeToggle = false
-                    )
+                    Column {
+                        DatePicker(
+                            state = datePickerState,
+                            showModeToggle = false
+                        )
+
+
+                        Button(
+                            onClick = {
+                                // Actualizar la fecha seleccionada y cerrar el DatePicker
+                                datePickerState.selectedDateMillis?.let { millis ->
+                                    val newDate = convertMillisToDate(millis)
+                                    onDateSelected(newDate)
+                                }
+                                showDatePicker = false
+                            },
+                            modifier = Modifier
+                                .align(Alignment.End)
+                                .padding(top = 16.dp)
+                        ) {
+                            Text("OK")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+// Función para convertir milisegundos a una fecha en formato legible (dd/MM/yyyy por ejemplo)
 fun convertMillisToDate(millis: Long): String {
-    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
-    return formatter.format(Date(millis))
+    val formatter = java.text.SimpleDateFormat("dd/MM/yyyy", java.util.Locale.getDefault())
+    return formatter.format(java.util.Date(millis))
 }
 
-@Composable
-fun ShowInvestmentOptions(){
-    val options = listOf("Google", "Apple", "Microsoft")
-    var selectedOption by remember { mutableStateOf("Select an option") }
 
-    Box(modifier = Modifier.fillMaxWidth()
-        .border(width = 1.dp, color = Color.Gray, shape = RectangleShape)
+
+@Composable
+fun ShowInvestmentOptions(selectedCompanies: List<String>, onCompaniesSelected: (List<String>) -> Unit) {
+    val options = listOf("GOOGL", "AAPL", "IBM")
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Gray, shape = RectangleShape)
+            .clickable { expanded = !expanded }
     ) {
         Text(
-            text = selectedOption,
-            modifier = Modifier
-                .padding(16.dp),
-            )
+            text = if (selectedCompanies.isEmpty()) "Select options" else selectedCompanies.joinToString(", "),
+            modifier = Modifier.padding(16.dp)
+        )
 
-        DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             options.forEach { option ->
-                DropdownMenuItem(text = { option },
-                    onClick = { selectedOption = option })
+                val isSelected = selectedCompanies.contains(option)
+                DropdownMenuItem(
+                    text = {
+                        Row {
+                            Text(option)
+                            Spacer(Modifier.weight(1f))
+                            if (isSelected) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                    },
+                    onClick = {
+                        val newCompanies = if (isSelected) {
+                            selectedCompanies - option
+                        } else {
+                            if (selectedCompanies.size < 3) {
+                                selectedCompanies + option
+                            } else {
+                                selectedCompanies
+                            }
+                        }
+                        onCompaniesSelected(newCompanies)
+                    }
+                )
             }
         }
     }
 }
+
 
 @Composable
 fun ShowSimulationGraph(){}
 
 @Composable
-fun ShowSimulationResult(){
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-    ) {
-        Column(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+fun ShowSimulationResult(analyticsResponse: AnalyticsResponse?) {
+        val latestResults =
+            analyticsResponse?.payload?.return_calculations?.mean?.runningMean?.map { (symbol, values) ->
+                val latestDate = values.keys.maxByOrNull { it }
+                val latestMean = latestDate?.let { values[it] }
+
+                symbol to (latestDate to latestMean)
+            }
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
         ) {
-            Text(
-                text = "Result",
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.padding(bottom = 8.dp)
-            )
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             ) {
                 Text(
-                    text = "+35 $",
-                    style = MaterialTheme.typography.titleLarge,
-                    modifier = Modifier.padding(end = 8.dp)
+                    text = "Investment Results",
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = 8.dp)
                 )
-                Text(
-                    text = "+0.4%",
-                    style = MaterialTheme.typography.titleLarge
-                )
+
+                latestResults?.forEach { (symbol, result) ->
+                    val (latestDate, latestMean) = result
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "$symbol (${latestDate ?: "No date"})",
+                            style = MaterialTheme.typography.titleLarge,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        Text(
+                            text = "${
+                                latestMean?.let {
+                                    String.format(
+                                        "%.2f",
+                                        it * 100
+                                    )
+                                } ?: "No data"
+                            }%",
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+                }
             }
         }
-    }
+
 }
 
 
